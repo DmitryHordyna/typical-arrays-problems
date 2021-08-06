302f7306565493caf87662a1210bc67b74d93369
DmitryHordyna <71545910+DmitryHordyna@users.noreply.github.com>
2021-08-06 21:11:01 +0200

    init:start

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  let min=array[0]
+  for (let i = 0; i < array.length; i += 1){
+    if (min > array[i]) {
+      min=array[i]
+    }
+    
+  }
+return array.length>0?min:0
 }
 
 exports.max = function max (array) {
-  return 0;
+   let max=array[0]
+  for (let i = 0; i < array.length; i += 1){
+    if (max < array[i]) {
+      max=array[i]
+    }
+    
+  }
+return array===0?0:max
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let total=0
+  for (let i = 0; i < array.length; i += 1){
+   total+=array[i] 
+  }
+  return array? total/array.length:0
+
 }
