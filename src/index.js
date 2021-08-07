f2a94fc4170f4cf5f94306a6829ebc8d32407a46
DmitryHordyna <71545910+DmitryHordyna@users.noreply.github.com>
2021-08-07 15:38:09 +0200

    fix:add return before star cycle if

--- src/index.js
+++ src/index.js
@@ -1,31 +1,49 @@
 
 exports.min = function min(array) {
-  let min=array[0]
-  for (let i = 0; i < array.length; i += 1){
-    if (min > array[i]) {
-      min=array[i]
+  if (typeof array === "undefined") {
+    return 0
+  } else if  (array.length === 0){
+    return 0
+  }else {
+    let min = array[0]
+    for (let i = 0; i < array.length; i += 1) {
+      if (min > array[i]) {
+        min = array[i]
+      }
     }
-    
+    return min
   }
-return array.length>0?min:0
+
 }
 
-exports.max = function max (array) {
+exports.max = function max(array) {
+    if (typeof array==="undefined" ) {
+    return 0
+    } else if (array.length === 0) {
+      return 0
+    }else {
    let max=array[0]
-  for (let i = 0; i < array.length; i += 1){
-    if (max < array[i]) {
-      max=array[i]
-    }
-    
+      for (let i = 0; i < array.length; i += 1) {
+        if (max < array[i]) {
+          max = array[i]
+        }
+      }
+      return max
   }
-return array===0?0:max
+
 }
 
 exports.avg = function avg (array) {
+  if (typeof array==="undefined" ) {
+    return 0
+  } else if (array.length === 0) {
+    return 0
+  }else {
   let total=0
   for (let i = 0; i < array.length; i += 1){
    total+=array[i] 
   }
-  return array? total/array.length:0
+  return  total / array.length
 
+  }
 }
